13f84459867e76f746648b11fcb1850a76f141a4
Joneshbot <joneshshrestha69@gmail.com>
2018-05-16 18:48:19 +0545

    DDA line drawing algorithm

--- sketch.js
+++ sketch.js
@@ -3,7 +3,8 @@ let x1, y1, x2, y2
 function setup() {
 	createCanvas(windowWidth,windowHeight)
 	background(211)
-	direct_line_drawing(10, 10, 150, 180)
+	//direct_line_drawing(10, 10, 150, 180)
+	DDA_line_drawing(10, 10, 150, 180)
 }
 
 function draw() {
@@ -27,4 +28,25 @@ function direct_line_drawing(x1, y1, x2, y2) {
 			point (x, y)
 		}
 	}
+}
+
+function DDA_line_drawing(x1, y1, x2, y2) {
+	let dx = x2 - x1
+	let dy = y2 - y1
+	let stepSize
+	if (abs(dy) > abs(dx)) {
+		stepSize = dy
+	} else {
+		stepSize = dx
+	}
+	let xInc = dx / stepSize
+	let yInc = dy / stepSize
+	let x = x1, y = y1
+	point(x, y)
+	while (x < x2 && y < y2) {
+		x = x + xInc
+		y = y + yInc
+		point(x, y)
+	}
+
 }
