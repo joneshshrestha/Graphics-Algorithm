9e382f70ad5239e18b9232a4604eedd773ef2813
Joneshbot <joneshshrestha69@gmail.com>
2018-05-16 21:39:04 +0545

    bresenhams line drawing algorithm with slope < 1

--- sketch.js
+++ sketch.js
@@ -4,7 +4,8 @@ function setup() {
 	createCanvas(windowWidth,windowHeight)
 	background(211)
 	//direct_line_drawing(10, 10, 150, 180)
-	DDA_line_drawing(10, 10, 150, 180)
+	//DDA_line_drawing(10, 10, 150, 180)
+	bresenham_line_drawing(10, 10, 150, 180)
 }
 
 function draw() {
@@ -47,6 +48,30 @@ function DDA_line_drawing(x1, y1, x2, y2) {
 		x = x + xInc
 		y = y + yInc
 		point(x, y)
+	}
+
+}
+
+function bresenham_line_drawing(x1, y1, x2, y2) {
+	let dx = x2 - x1
+	let dy = y2 - y1
+	let p = 2 * dy - dx
+	let x = x1, y = y1
+	let i = 1
+	point(x, y)
+	while (i <= dx) {
+		if (p < 0) {
+			x = x + 1
+			p = p + 2 * dy
+			point(x, y)
+			i++
+		} else {
+			x = x + 1
+			y = y + 1
+			p = p + (2 * dy) - (2 * dx)
+			point(x, y)
+			i++
+		}	
 	}
 
 }
