a5e5c9228158a4c100e1aa5cdcc90d30564309cf
Jonesh Shrestha <joneshshrestha69@gmail.com>
2018-06-03 07:41:45 +0545

    update

--- piechart/sketch.js
+++ piechart/sketch.js
@@ -1,13 +1,17 @@
 let xc, yc, r
 let x1, y1, x2, y2
-let diameter, data
 
 function setup() {
 	createCanvas(windowWidth,windowHeight)
 	background(211)
+	stroke(0, 0, 0)
 	midpoint_circle_drawing(500, 350, 250)
-	//bresenham_line_drawing(500, 350, 600, 173)
-	bresenham_line_drawing(500, 350, 550, 650)
+	stroke(0, 0, 255)
+	bresenham_line_drawing(500, 350, 650, 153)
+	stroke(255, 0, 0)
+	bresenham_line_drawing(500, 350, 450, 595)
+	stroke(0, 255, 0)
+	bresenham_line_drawing(500, 350, 255, 300)
 }
 
 function midpoint_circle_drawing(xc, yc, r) {
@@ -47,7 +51,7 @@ function bresenham_line_drawing(x1, y1, x2, y2) {
 	let i = 1
 	point(x, y)
 	if (abs(m) < 1) {
-		let p = (2 * dy) - dx;
+		let p = (2 * dy) - dx
 		while (i <= dx) {
 			if (p < 0) {
 				x = x + 1
@@ -61,7 +65,7 @@ function bresenham_line_drawing(x1, y1, x2, y2) {
 			i++
 		}
 	} else { 
-		let p = (2 * dx) - dy;
+		let p = (2 * dx) - dy
 		while (i <= dy) {
 			if (p < 0) {
 				y = y + 1
@@ -69,21 +73,11 @@ function bresenham_line_drawing(x1, y1, x2, y2) {
 			} else {
 				x = x + 1
 				y = y + 1
-				p = p + (2 * dx) - (2 * dy);
+				p = p + (2 * dx) - (2 * dy)
 			}
 			point(x, y)
-			i++;
+			i++
 		}
-	}
-}
-
-function pie_chart(diameter, data) {
-	let lastAngle = 0;
-	for (let i = 0; i < data.length; i++) {
-	  let gray = map(i, 0, data.length, 0, 255);
-	  fill(gray);
-	  arc(width/2, height/2, diameter, diameter, lastAngle, lastAngle+radians(angles[i]));
-	  lastAngle += radians(angles[i]);
 	}
 }
 
